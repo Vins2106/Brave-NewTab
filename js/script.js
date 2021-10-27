fa0887dad0cb4a025e3fc7382f183a1801d3be87
Vins 2106 <69247128+Vins2106@users.noreply.github.com>
2021-10-27 19:23:01 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let config = [
+	{name: "github", link: 'https://github.com/Vins2106'},
+	{name: "github-notif", link: 'https://github.com/notification'},
+	{name: "gmail", link: 'https://gmail.com'},
+	{name: "youtube", link: 'https://youtube.com'},
+	{name: "youtube-channel", link: 'https://youtube.com/c/Vins2106'},
+	{name: "own", link: 'https://vinsdev.xyz'}
+]
+
+let quotes = [
+	"Any fool can write code that a computer can understand.",
+	"First, solve the problem.",
+	"Experience is the name everyone gives to their mistakes.",
+	" In order to be irreplaceable, one must always be different.",
+	"Java is to JavaScript what car is to Carpet.",
+	"Knowledge is power.",
+]
+
+let quoteDock = document.getElementById("quotes");
+
+quoteDock.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+
+setInterval(() => {
+	quoteDock.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+}, 2 * 60000)
+
+
+function winOpen(name) {
+	let find = config.find(x => x.name === name);
+	if (!find) return;
+
+	location.href = find.link
+}
