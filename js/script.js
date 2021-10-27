8c6f75f5f80f50361c497decbbac45d9e2118b6c
Vins 2106 <69247128+Vins2106@users.noreply.github.com>
2021-10-27 19:46:11 +0800

    add: time and copyright

--- js/script.js
+++ js/script.js
@@ -44,3 +44,22 @@ form.addEventListener("submit", (e) => {
 	location.href = googleSearch + search
 	return;
 })
+
+let time = document.getElementById("time");
+
+time.innerHTML = formatAMPM(new Date());
+
+setInterval(() => {
+	time.innerHTML = formatAMPM(new Date())
+}, 1 * 1000)
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
